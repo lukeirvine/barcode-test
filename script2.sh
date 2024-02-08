c01b3fc6205434695cbdcd11c0b4757384dd3f86
lukeirvine <lukeirvinefreelance@gmail.com>
2024-02-08 15:35:23 -0800

    simplified paths

--- script2.sh
+++ script2.sh
@@ -3,9 +3,9 @@
 conda create -p ./venv-python3.12 python=3.12 -y
 conda activate ./venv-python3.12
 sudo yum install mesa-libGL
-pip3 install --target=./venv-python3.12/lib/python3.12 requests
-pip3 install --target=./venv-python3.12/lib/python3.12 opencv-python
-conda install -p ./venv-python3.12 conda-forge::pyzbar
+pip3 install --target=./python requests
+pip3 install --target=./python opencv-python
+conda install -p ./python conda-forge::pyzbar
 bash copy-binaries.sh
 # show path dependencies for opencv-python to make sure none are coming from /lib64
-ldd ./venv-python3.12/lib/python3.12/cv2/cv2.abi3.so
+ldd ./python/cv2/cv2.abi3.so
