80412d55e1ecfb0f2969d3e1ae6fa8d524d248f9
lukeirvine <lukeirvinefreelance@gmail.com>
2024-02-08 13:04:09 -0800

    updated script2.sh to show dependencies of opencv-python

--- script2.sh
+++ script2.sh
@@ -6,3 +6,5 @@ pip3 install --target=./venv-python3.12/lib/python3.12 requests
 pip3 install --target=./venv-python3.12/lib/python3.12 opencv-python
 conda install -p ./venv-python3.12 conda-forge::pyzbar
 bash copy-binaries.sh
+# show path dependencies for opencv-python to make sure none are coming from /lib64
+ldd ./venv-python3.12/lib/python3.12/cv2/cv2.abi3.so
