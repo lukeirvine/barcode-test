f44daba71ee57e9a68836dd02025d8cfe20eae94
lukeirvine <lukeirvinefreelance@gmail.com>
2024-02-08 13:20:29 -0800

    updated script2.sh

--- script2.sh
+++ script2.sh
@@ -2,6 +2,7 @@
 
 conda create -p ./venv-python3.12 python=3.12 -y
 conda activate ./venv-python3.12
+sudo yum install mesa-libGL
 pip3 install --target=./venv-python3.12/lib/python3.12 requests
 pip3 install --target=./venv-python3.12/lib/python3.12 opencv-python
 conda install -p ./venv-python3.12 conda-forge::pyzbar
