6046f205d4219e081d818fce8c2f5a57664793f9
lukeirvine <lukeirvinefreelance@gmail.com>
2024-02-08 13:16:46 -0800

    updated paths in copy script

--- copy-binaries.sh
+++ copy-binaries.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 # Copy all necessary binaries for opencv-python to local installation
-cp /lib64/libz.so.1 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libpthread.so.0 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/librt.so.1 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libdl.so.2 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libstdc++.so.6 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libm.so.6 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libgcc_s.so.1 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libc.so.6 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/ld-linux-x86-64.so.2 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libgthread-2.0.so.0 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libglib-2.0.so.0 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libpcre2-8.so.0 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libGL.so.1 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libGLX.so.0 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libX11.so.6 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libXext.so.6 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libGLdispatch.so.0 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libxcb.so.1 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
-cp /lib64/libXau.so.6 /home/ec2-user/environment/my-lambda/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libz.so.1 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libpthread.so.0 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/librt.so.1 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libdl.so.2 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libstdc++.so.6 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libm.so.6 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libgcc_s.so.1 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libc.so.6 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/ld-linux-x86-64.so.2 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libgthread-2.0.so.0 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libglib-2.0.so.0 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libpcre2-8.so.0 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libGL.so.1 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libGLX.so.0 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libX11.so.6 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libXext.so.6 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libGLdispatch.so.0 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libxcb.so.1 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
+cp /lib64/libXau.so.6 /home/ec2-user/environment/barcode-test/venv-python3.12/lib/python3.12/opencv_python.libs/
